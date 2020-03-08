0440352d074c02cd9184e755e6840aa8ba9432e2
NohaMahgoub <noha.mahgou@gmail.com>
2020-03-08 16:38:55 +0300

    create order v1

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -20,86 +20,118 @@ class OrderController extends APIController
  //======================== create order  ======================
  public function store($user_id,Request $request)
  {
-    //find user
+    //find user======================================
     $user = User::findOrFail($user_id);
-    //get product id and price
+    //get product id,price,quantity================== 
     $product_id = $request->product_id;
     $myProduct = Product::findOrFail($product_id);
     $product_price = $myProduct->price;
-    //get platform id if empty set null
+    $products_quantity = $request->products_quantity;
+    //get platform id if empty set null==============
     $platform_id = $request->platform_id;
     $myPlatform = Platform::find($platform_id);
     if(!$myPlatform){
     $platform_id =Null;
     }
-    // //get position id if empty set null
-    // $position_id = $request->position_id;
-    // $myPosition = Position::find($position_id);
-    // if(!$myPosition){
-    // $position_id =Null;
-    // }
-    // //get music id if empty set null
-    // $music_id = $request->music_id;
-    // $myMusic = MusicSample::find($music_id);
-    // if(!$myMusic){
-    // $music_id =Null;
-    // }
-    // // set defult order status 0->(pending)
-    // $status_id = 0;
+    //get template id if empty set null===============
+    $template_id = $request->template_id;
+    $myTemplate = Template::find($template_id);
+    if(!$myTemplate){
+    $template_id =Null;
+    }
+    //get music id if empty set null====================
+    $music_id = $request->music_id;
+    $myMusic = MusicSample::find($music_id);
+    if(!$myMusic){
+    $music_id =Null;
+    }
+    //get logo of order if empty set null===============
+    if($request->hasFile('logo')){
+     // Get filename with the extension
+    $filenameWithExt = $request->file('logo')->getClientOriginalName();
+    // Get just filename
+    $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+     // Get just ext
+     $extension = $request->file('logo')->getClientOriginalExtension();
+    // Filename to store
+    $fileNameToStore= $filename.'_'.time().'.'.$extension;
+    // Upload Image
+    $path = $request->file('logo')->storeAs('public/order_logo', $fileNameToStore);
+    } else {
+    $fileNameToStore = null;
+    }
+    // set defult order status 1->(pending)==============
+    $status_id = 1;
+    // get vedio lenght if  empty set null================
+    $video_length = $request->video_length;
+    if(!$video_length){
+    $video_length =Null;
+        }
+    // get notes if  empty set null========================
+    $notes = $request->notes;
+    if(!$notes){
+    $notes =Null;
+    }
+    //get addon id,price if empty set 0=====================
+    $addon_id = $request->addon_id;
+    $Myaddon= Addon::find($addon_id);
+    if(!$Myaddon){
+        $addon_id =Null;
+        $addonPrice =0;
+        }else{
+        $addonPrice= $Myaddon->price;
+        }
+    //get coupon id,price if empty set 0=====================   
+    $coupon_code = $request->coupon_code;
+    $MyCoupon= Coupon::where('code',$coupon_code)->get();
+    $validity= Coupon::where('code',$coupon_code)->value('valid');
+    $discount= Coupon::where('code',$coupon_code)->value('amount');
+    $quantity= Coupon::where('code',$coupon_code)->value('quantity');
+    if($validity==0){
+    return "invalid coupon";
+    }
+    if($quantity<1){
+    return "invalid coupon";
+    }
+    if(!$MyCoupon){
+        $coupon_code =Null;
+        $discount= 0;
+    }
+   //put default value for payment id===========================   
+    $payment_id = 0;
+   //get Location id============================================
+   $location_id = $request->location_id;
+   $myLocation = Location::find($location_id);
+   if(!$myLocation){
+    $location_id =Null;
+    }
+
+    //calculate total_price ====================================
+    $total_price1= (($product_price*$products_quantity)+$addonPrice);
+    $discountValue= $discount/100;
+    $total_price= $total_price1*(1-$discountValue);
+
+
+
+ try{
     
+     $data = $request->only('user_id','product_id','platform_id','addon_id',
+     'location_id','coupon_id','music_id','status_id','products_quantity','video_length'
+    ,'notes');
+     $orderData = array_merge($data , ['image' => $fileNameToStore]);
+     $pakageOrder = Order::create($orderData);
+     return $pakageOrder;    
+
+     
+
+
+ } catch(\Illuminate\Database\QueryException $e){
+     $errorCode = $e->errorInfo[1];
+     if($errorCode == '1062'){
+         return response()->json("this order is already registered!" );
+     }}
 
 
 
-return $product_price;
-
-
-//     //get addon id
-//     $addon_id = Addon::findOrFail($addon_id); 
-//     //get coupon id   
-//     $coupon_id = Coupon::findOrFail($user_id);
-//     ///put default value for status id   
-//     $status_id = 1;
-//     //get musicSample id   
-//     $music_id = MusicSample::findOrFail($user_id);
-//     //put default value for payment id   
-//     $payment_id = 0;
-
-
-
-
-// //get Location id
-// $location_id = Location::findOrFail($location_id);
-
-//  try{
-//     if($request->hasFile('logo')){
-//         // Get filename with the extension
-//         $filenameWithExt = $request->file('logo')->getClientOriginalName();
-//         // Get just filename
-//         $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-//         // Get just ext
-//         $extension = $request->file('logo')->getClientOriginalExtension();
-//         // Filename to store
-//         $fileNameToStore= $filename.'_'.time().'.'.$extension;
-//         // Upload Image
-//         $path = $request->file('logo')->storeAs('public/order_logo', $fileNameToStore);
-//     } else {
-//         $fileNameToStore = 'noimage.jpg';
-//     }
-
-
-//      $order = new Order;
-//      $order->user_id= $user_id;
-    
-
-//      $order->image= $fileNameToStore;
-//      $order->save();
-//      return response()->json($order);
-
-
-//  } catch(\Illuminate\Database\QueryException $e){
-//      $errorCode = $e->errorInfo[1];
-//      if($errorCode == '1062'){
-//          return response()->json("this order is already registered!" );
-//      }}
  }
 }
--- app/Models/Order/Order.php
+++ app/Models/Order/Order.php
@@ -8,4 +8,11 @@ class Order extends Model
 {
     protected $table='orders';
 
+
+    protected $fillable = [
+     'product_id','platform_id','addon_id',
+        'location_id','coupon_id','music_id','status_id','products_quantity','video_length'
+       ,'notes','product_price'
+    ];
+    
 }

ae0b638e12344dab33fcbb13256e50f4f94252af
Mohammed Khalaf <mohammedkhalf.2022@gmail.com>
2020-04-26 21:15:06 -0700

    enhancement location

--- app/Http/Controllers/Api/V1/LocationController.php
+++ app/Http/Controllers/Api/V1/LocationController.php
@@ -3,76 +3,63 @@
 namespace App\Http\Controllers\Api\V1;
 use Illuminate\Http\Request;
 use App\Models\Location\Location;
-use App\Models\Access\User\User;
 
 
 class LocationController extends APIController
 {
 
-//======================== index location  ======================
-public function index()
-{
-     $user = Location::where('user_id',auth()->user()->id)->get();
-     return response()->json($user);
-}
-//======================== create location  ======================
-public function store(Request $request)
-{
-   $this->validate($request,[
-      'country'=> 'required',
-      'city'=> 'required',
-      ]);
-try{
+      //======================== index location  ======================
+      public function index()
+      {
+         $userLocations = Location::where('user_id',auth()->user()->id)->get();
+         return response()->json($userLocations);
+      }
+      //======================== create location  ======================
+      public function store(Request $request)
+      {     
+            if(Location::where('user_id','=',auth()->user()->id)->first())
+            {
+               return response()->json("location Already susseccfully!");
+            }
+         
+            $this->validate($request,[
+                  'country'=> 'string|required',
+                  'city'=> 'string|required',
+               ]);
+            $location = new Location();
+            $LocationInfo = $request->only('country','city','address','postal_code','unit_no','state','lng','lat');
+            $LocationArray = array_merge($LocationInfo , ['user_id'=>auth()->user()->id]);
+            $location->update($LocationArray);
+            return response()->json("location created susseccfully!");
+      }
+      //======================== show location  ======================
 
-$location = new Location();
+      public function show($id)
+      {
+         $location = Location::findOrFail($id);
+         return response()->json($location);
+      }
+      //======================== update location  ======================
 
-$location->create([
-   'country' => $request->country,
-   'city' => $request->city,
-   'address' => $request->address,
-   'postal_code' => $request->postal_code,
-   'unit_no' => $request->unit_no,
-   'state' => $request->state,
-   'lng' => $request->lng,
-   'lat' => $request->lat,
-   'user_id' => (auth()->user()->id),
-   ]);  
-return response()->json("location created susseccfully!");
+      public function update(Request $request, $id)
+      {
+         $this->validate($request,[
+            'country'=> 'required',
+            'city'=> 'required',
+            ]);
 
-} catch(\Illuminate\Database\QueryException $e){
-$errorCode = $e->errorInfo[1];
-if($errorCode == '1062'){
-   return response()->json("this location is already registered!" );
-}}
-}
-//======================== show location  ======================
+         $location = Location::findOrFail($id);
+         $locationData = $request->only('country','city','address','unit_no','postal_code','state','lng','lat');
+         $location->update($locationData);
+         return response()->json($location);
+      }
 
-public function show($id)
-{
- $location = Location::findOrFail($id);
- return response()->json($location);
-}
-//======================== update location  ======================
+   //======================== delete location  ======================
 
-public function update(Request $request, $id)
-{
-   $this->validate($request,[
-      'country'=> 'required',
-      'city'=> 'required',
-      ]);
-       $location = Location::findOrFail($id);
-       $locationData = $request->only('country','city','address','unit_no','postal_code','state','lng','lat');
-       $location->update($locationData);
-       return response()->json($location);
-          }
-
-//======================== delete location  ======================
-
-public function destroy($id)
-{
-$location = Location::findOrFail($id);
-$location -> delete();  
-return response()->json("deleted successfully");
-
-}
+      public function destroy($id)
+      {
+         $location = Location::findOrFail($id);
+         $location->delete();  
+         return response()->json("Location deleted successfully");
+      }
 }

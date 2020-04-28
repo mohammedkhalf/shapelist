8a16b0e842df4ae341b78610e3581e7ba1b8b11e
Mohammed Khalaf <mohammedkhalf.2022@gmail.com>
2020-04-27 17:47:02 -0700

    add rep to locations

--- app/Http/Controllers/Api/V1/LocationController.php
+++ app/Http/Controllers/Api/V1/LocationController.php
@@ -17,7 +17,7 @@ class LocationController extends APIController
       //======================== create location  ======================
       public function store(Request $request)
       {     
-            if(Location::where('user_id','=',auth()->user()->id)->first())
+            if(Location::where('id','=',$request->id)->first())
             {
                return response()->json("location Already susseccfully!");
             }
@@ -25,15 +25,27 @@ class LocationController extends APIController
             $this->validate($request,[
                   'country'=> 'string|required',
                   'city'=> 'string|required',
+                  'address'=>'string|nullable',
+                  'postal_code'=>'string|nullable',
+                  'unit_no'=>'string|nullable',
+                  'lng'=>'string|nullable',
+                  'lat'=>'string|nullable',
+                  'rep_first_name'=>'string|nullable',
+                  'rep_last_name'=>'string|nullable',
+                  'rep_phone_number'=>'string|nullable',
+
                ]);
             $location = new Location();
-            $LocationInfo = $request->only('country','city','address','postal_code','unit_no','state','lng','lat');
+            $LocationInfo = $request->only('country','city','address','postal_code','unit_no',
+                                    'lng','lat','rep_first_name','rep_last_name','rep_phone_number');
             $LocationArray = array_merge($LocationInfo , ['user_id'=>auth()->user()->id]);
-            $location->update($LocationArray);
-            return response()->json("location created susseccfully!");
+            $location = Location::create($LocationArray);
+            return response()->json([
+                     'message'=>'location created susseccfully',
+                     'location' => $location
+            ]);
       }
       //======================== show location  ======================
-
       public function show($id)
       {
          $location = Location::findOrFail($id);
@@ -43,15 +55,23 @@ class LocationController extends APIController
 
       public function update(Request $request, $id)
       {
-         $this->validate($request,[
-            'country'=> 'required',
-            'city'=> 'required',
+            $this->validate($request,[
+               'country'=> 'string|required',
+               'city'=> 'string|required',
+               'address'=>'string|nullable',
+               'postal_code'=>'string|nullable',
+               'unit_no'=>'string|nullable',
+               'lng'=>'string|nullable',
+               'lat'=>'string|nullable',
+               'rep_first_name'=>'string|nullable',
+               'rep_last_name'=>'string|nullable',
+               'rep_phone_number'=>'string|nullable',
             ]);
-
          $location = Location::findOrFail($id);
-         $locationData = $request->only('country','city','address','unit_no','postal_code','state','lng','lat');
+         $locationData = $request->only('country','city','address','unit_no',
+                                 'postal_code','lng','lat','rep_first_name','rep_last_name','rep_phone_number');
          $location->update($locationData);
-         return response()->json($location);
+         return response()->json(['message'=>'location created susseccfully','location'=>$location]);
       }
 
    //======================== delete location  ======================
@@ -60,6 +80,6 @@ class LocationController extends APIController
       {
          $location = Location::findOrFail($id);
          $location->delete();  
-         return response()->json("Location deleted successfully");
+         return response()->json("Location Deleted Successfully");
       }
 }
--- app/Models/Location/Location.php
+++ app/Models/Location/Location.php
@@ -33,7 +33,8 @@ class Location extends Model
      * @var array
      */
     protected $fillable = [
-        'country', 'city','address', 'postal_code','unit_no','state','lng','lat','user_id','order_id'
+        'country', 'city','address', 'postal_code','unit_no','state'
+        ,'lng','lat','user_id','order_id','rep_first_name','rep_last_name','rep_phone_number'
     ];
 
     /**

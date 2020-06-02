8a75080c7100da3af547cd453565b9638dc48d58
NohaMahgoub <noha.mahgou@gmail.com>
2020-06-02 12:07:05 +0300

    create end point for vat and onSet

---
+++ app/Http/Controllers/Api/V1/QuotationController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+use App\Models\Quotation\Quotation;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class QuotationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function vat()
+    {
+        $vat = Quotation::where('name', 'VAT')->get();
+        return response()->json($vat);
+    }
+
+     public function onSet()
+    {
+        $onSet = Quotation::where('name', 'On-Set')->get();
+        return response()->json($onSet);    }
+}

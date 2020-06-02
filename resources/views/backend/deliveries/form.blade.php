24d74d85d1bc73aaa8cff45d0e69aef95cfdb510
NohaMahgoub <noha.mahgou@gmail.com>
2020-06-02 13:47:13 +0300

    create form for delivery

--- resources/views/backend/deliveries/form.blade.php
+++ resources/views/backend/deliveries/form.blade.php
@@ -1,13 +1,16 @@
 <div class="box-body">
     <div class="form-group">
-        <!-- Create Your Field Label Here -->
-        <!-- Look Below Example for reference -->
-        {{-- {{ Form::label('name', trans('labels.backend.blogs.title'), ['class' => 'col-lg-2 control-label required']) }} --}}
+        {{ Form::label('name', trans('labels.backend.deliveries.table.name'), ['class' => 'col-lg-2 control-label required']) }}
 
         <div class="col-lg-10">
-            <!-- Create Your Input Field Here -->
-            <!-- Look Below Example for reference -->
-            {{-- {{ Form::text('name', null, ['class' => 'form-control box-size', 'placeholder' => trans('labels.backend.blogs.title'), 'required' => 'required']) }} --}}
+         {{ Form::text('name', old('name'), ['class' => 'form-control box-size', 'placeholder' => trans('labels.backend.deliveries.table.name'), 'required' => 'required']) }}
+        </div><!--col-lg-10-->
+    </div><!--form-group-->
+    <div class="form-group">
+        {{ Form::label('price', trans('labels.backend.deliveries.table.price'), ['class' => 'col-lg-2 control-label required']) }}
+
+        <div class="col-lg-10">
+         {{ Form::text('price', old('price'), ['class' => 'form-control box-size', 'placeholder' => trans('labels.backend.deliveries.table.price'), 'required' => 'required']) }}
         </div><!--col-lg-10-->
     </div><!--form-group-->
 </div><!--box-body-->

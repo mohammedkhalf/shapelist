fe0df01f2612bd895e2f2cff8445726c2238ede3
NohaMahgoub <noha.mahgou@gmail.com>
2020-06-03 11:36:04 +0300

    fix label and menu2

--- resources/views/backend/payments/form.blade.php
+++ resources/views/backend/payments/form.blade.php
@@ -1,20 +1,21 @@
 <div class="box-body">
-    <div class="form-group">
-       
-        {{ Form::label('status', trans('labels.backend.payments.table.status'), ['class' => 'col-lg-2 control-label required']) }}
+            <div class="form-group">
+            
+                {{ Form::label('status', trans('labels.backend.payments.table.status'), ['class' => 'col-lg-2 control-label required']) }}
 
-        <div class="col-lg-2">
+                <div class="col-lg-2">
 
-            <select class="form-control" name="status" id="status" onchange="showReason()" >
-                @if(isset($payment))
-                <option value="1" {{ old('status') == "True" ? 'selected' : '' }}> True </option>
-                <option value="2" {{ old('status') == "False" ? 'selected' : '' }}> False </option>
-                @else
-                  none
-                @endif
-            </select>
-        </div><!--col-lg-10-->
-    </div><!--form-group-->
+                    <select class="form-control" name="status" id="status" onchange="showReason()" >
+                        @if(isset($payment))
+                            <option value="1" {{ old('status') == 1 ? 'selected' : '' }}> True </option>
+                            <option value="2" {{ old('status') == 2 ? 'selected' : '' }}> False </option>
+                        @else
+                        none
+                        @endif
+                    </select>
+                </div><!--col-lg-10-->
+            </div><!--form-group-->
+
             <div class="form-group" id ="reason" style="display: none">
             
                 {{ Form::label('failure_reason', trans('labels.backend.payments.table.failure_reason'), ['class' => 'col-lg-2 control-label ']) }}
@@ -28,9 +29,6 @@
 
 @section("after-scripts")
     <script type="text/javascript">
-        //Put your javascript needs in here.
-        //Don't forget to put `@`parent exactly after `@`section("after-scripts"),
-        //if your create or edit blade contains javascript of its own
         $( document ).ready( function() {
             document.getElementById("reason").style.display = "none";
             $status=  document.getElementById("status").value;
@@ -38,13 +36,13 @@
 
         function showReason() {
             $element = document.getElementById("status").value;
-          if ($element == 2){
-          document.getElementById("reason").style.display = "block";
+            if ($element == 2){
+                 document.getElementById("reason").style.display = "block";
             }
             if ($element == 1){
-          document.getElementById("reason").style.display = "none";
+                  document.getElementById("reason").style.display = "none";
             }
-              }
+        }
 
     </script>
 @endsection

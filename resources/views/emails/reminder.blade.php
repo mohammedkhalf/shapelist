19aef76877cff5892ad13581947d5086e7719a78
NohaMahgoub <noha.mahgou@gmail.com>
2020-06-23 15:00:48 +0300

    complete all reminders

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -7,18 +7,19 @@
         <div class="fd-header center">
                 <div class="fd-email-image" ></div>
                 <div class="fd-welcome-text">
-                <p style="font-size:15px ; text-align:center; font-weight: 600;"> Hello, {{$subscriber->user->first_name}} {{$subscriber->user->last_name}}</p>
-                    <p style="font-size:13px ; text-align:center; font-weight: lighter; color:#FFFFFF"> Subscriptions Reminder </p>
+                <p style="font-size:15px ; text-align:center; font-weight: 600; opacity: 0.8;"> Hello, {{$subscriber->user->first_name}} {{$subscriber->user->last_name}}!</p>
+                    <p style="font-size:13px ; text-align:center; font-weight: lighter; color:#FFFFFF ;opacity: 0.8;"> Subscriptions Reminder </p>
                 </div>       
         </div>
-
-        <div class="fd-email-body"  style=" color: #000000e3">
-            <p class="fd-email-text"> Your subscription is about to expire, Visit your dashboard and renew your subscription to keep your points! </p>
+       
+        <div class="fd-email-body"  style="color: #000000a1; font-weight: 700; padding-top: 18px !important;">
+            <p class="fd-email-text"  style="color: #000000a1; opacity: 0.8;">  Your subscription is about to expire,</p>
+            <p style="color: #000000a1;opacity: 0.8;"> Visit your dashboard and renew your subscription to keep your points! </p>
             <div class="fd-button center" style="margin-top: 30px !important;">
-                <a href="#" style="color: #ffffff; text-decoration: none;margin-top: 15px;"> Visit your dashboard </a>
+                <a href="#" style="color: #ffffff; text-decoration: none;margin-top: 15px;opacity: 0.8;"> Visit your dashboard </a>
             </div> 
-            <div class="fd-email-message" style=" color: #000000e3">
-                <p style="font-weight: 500;"> Also, if you need any help, do not hesitate to contact us anytime,<b><a style="text-decoration:none;color: #000000;"> info@shapelist.com </a></b></p>
+            <div class="fd-email-message" style="color: #000000a1;opacity: 0.8; font-weight: 700;">
+                <p> Also, if you need any help, do not hesitate to contact us anytime,<b><a style="text-decoration:none;color: #000000;"> info@shapelist.com </a></b></p>
                 <p>Best Wishes,<br> Shapelist Support Team.</p>
             </div>
         </div>

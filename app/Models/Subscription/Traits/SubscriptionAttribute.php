13c4ce030f3f9e5489868206e353c7617155631c
Mohammed Khalaf <mohammedkhalf.2022@gmail.com>
2020-06-24 19:23:24 -0700

    enhance subscription

---
+++ app/Models/Subscription/Traits/SubscriptionAttribute.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\Subscription\Traits;
+
+/**
+ * Class SubscriptionAttribute.
+ */
+trait SubscriptionAttribute
+{
+    // Make your attributes functions here
+    // Further, see the documentation : https://laravel.com/docs/6.x/eloquent-mutators#defining-an-accessor
+
+
+    /**
+     * Action Button Attribute to show in grid
+     * @return string
+     */
+    public function getActionButtonsAttribute()
+    {
+        return '<div class="btn-group action-btn"> '
+                 .$this->getEditButtonAttribute("edit-subscription", "admin.subscriptions.edit").'
+                '.$this->getListButtonAttribute("list-subscription", "admin.subscriptionDetails").'
+                '.$this->getDeleteButtonAttribute("delete-subscription", "admin.subscriptions.destroy").'
+               </div>';
+    }
+}

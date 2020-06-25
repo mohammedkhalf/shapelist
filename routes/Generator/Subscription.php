319ead5aa94bb2214f43354e582f501d07bfab65
Mohammed Khalaf <mohammedkhalf.2022@gmail.com>
2020-06-25 16:50:23 -0700

    fix subscribtion

---
+++ routes/Generator/Subscription.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Subscription
+ *
+ */
+Route::group(['namespace' => 'Backend', 'prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'admin'], function () {
+    
+    Route::group( ['namespace' => 'Subscription'], function () {
+        Route::resource('subscriptions', 'SubscriptionsController');
+        Route::get('/subscriptions/subscribers/{id}', 'SubscriptionsController@subscribers')->name('subscriptionDetails');
+        Route::get('/subscriptions/removeSubscriber/{id}', 'SubscriptionsController@removesubscriber')->name('subscriptions.removesubscriber');
+        Route::get('/unsubscriber', 'SubscriptionsController@unsubscribers')->name('subscriptions.unsubscribe');
+        //For Datatable
+        Route::post('subscriptions/get', 'SubscriptionsTableController')->name('subscriptions.get');
+
+
+        
+    });
+    
+});

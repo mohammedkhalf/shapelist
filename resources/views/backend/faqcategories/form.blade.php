9c9ec04d9a21328d7acf8dc6e3c1b3fd35863273
NohaMahgoub <noha.mahgou@gmail.com>
2020-06-28 11:50:31 +0300

    readonly in edit faq categories

--- resources/views/backend/faqcategories/form.blade.php
+++ resources/views/backend/faqcategories/form.blade.php
@@ -1,24 +1,13 @@
 <div class="box-body">
     <div class="form-group">
-        <!-- Create Your Field Label Here -->
-        <!-- Look Below Example for reference -->
         {{ Form::label('name', trans('labels.backend.faqcategories.table.name'), ['class' => 'col-lg-2 control-label required']) }}
 
         <div class="col-lg-10">
-            <!-- Create Your Input Field Here -->
-            <!-- Look Below Example for reference -->
-            {{ Form::text('name', old('name'), ['class' => 'form-control box-size', 'placeholder' => trans('labels.backend.faqcategories.table.name'), 'required' => 'required' ,'readonly' => 'true']) }}
-        </div><!--col-lg-10-->
+            @if(isset($faqcategory))
+              {{ Form::text('name', old('name'), ['class' => 'form-control box-size', 'placeholder' => trans('labels.backend.faqcategories.table.name'), 'required' => 'required' ,'readonly' => 'true']) }}
+            @else
+              {{ Form::text('name', old('name'), ['class' => 'form-control box-size', 'placeholder' => trans('labels.backend.faqcategories.table.name'), 'required' => 'required' ]) }}
+            @endif
+            </div><!--col-lg-10-->
     </div><!--form-group-->
 </div><!--box-body-->
-
-@section("after-scripts")
-    <script type="text/javascript">
-        //Put your javascript needs in here.
-        //Don't forget to put `@`parent exactly after `@`section("after-scripts"),
-        //if your create or edit blade contains javascript of its own
-        $( document ).ready( function() {
-            //Everything in here would execute after the DOM is ready to manipulated.
-        });
-    </script>
-@endsection

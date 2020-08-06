04c2ec83ab366089a8e793dbd99d186d9667b002
mohammed khalaf <mohammedkhalaf@mohammeds-MacBook-Air.local>
2020-08-06 20:30:49 +0200

    enhance validation

--- app/Http/Requests/Backend/Order/UpdateOrderRequest.php
+++ app/Http/Requests/Backend/Order/UpdateOrderRequest.php
@@ -30,34 +30,22 @@ class UpdateOrderRequest extends FormRequest
     public function rules()
     {
         return [
-            'delivery_id'=>['numeric','not_in:0','exists:'. Delivery::table() .',id'],
+            'delivery_id'=>['required','numeric','not_in:0','exists:'. Delivery::table() .',id'],
             'location_id'=>['numeric','not_in:0','exists:'. Location::table() .',id' , 'nullable'],
-            'total_price'=>['numeric' , 'not_in:0'],
+            'total_price'=>['required','numeric','not_in:0'],
             'coupon_code' => ['string','max:10' ,  new FilterStringRule , 'nullable'],
-            'on_set' =>['date_format:Y-m-d H:i:s' , 'nullable'],
-            'country'=>['string',new FilterStringRule , 'nullable'],
-            'city'=>['string',new FilterStringRule , 'nullable'], 
-            'address' =>['string', new FilterStringRule , 'nullable'],
-            'lat' =>['string', new FilterStringRule , 'nullable'],
-            'lng' =>['string', 'nullable'],
-            'rep_first_name'=>['string', new FilterStringRule , 'nullable'],
-            'rep_last_name'=>['string', new FilterStringRule , 'nullable'],
-            'rep_phone_number'=>['numeric','regex:/(0)[0-9]{9}/' , 'nullable'], 
-            //products array
-            'products.*.product_id' => ['numeric' , 'not_in:0' , 'exists:'. Product::table() .',id'],
-            'products.*.product_quantity' => ['numeric' , 'not_in:0'],
-            'products.*.product_total_price' => ['numeric','not_in:0'],
-            'products.*.video_length' => ['nullable','numeric','not_in:0'],
-            'products.*.music_id' => ['nullable','numeric' , 'not_in:0' , 'exists:'. MusicSample::table() .',id'],
-            'products.*.user_music' => ['nullable' , 'mimes:mpga,ogg'],
-            //package array
-            'packages.*.package_id'=>['numeric','not_in:0','exists:'. Package::table() .',id'],
-            'packages.*.quantity'=>['numeric','not_in:0'],
-            'packages.*.vedio_length' => ['nullable','numeric','not_in:0'],
-            'packages.*.package_music_id' => ['nullable','numeric' , 'not_in:0' , 'exists:'. MusicSample::table() .',id'],
-            'packages.*.package_user_music' => ['nullable','mimes:mpga,ogg'],
-            //zip folder
-            'zip_folder' => ['mimes:zip','max:20000']
+            'products.*.services' => ['required','string',new FilterStringRule],
+            'products.*.quantity' => ['numeric','integer','not_in:0'],
+            'products.*.productType' => ['string',new FilterStringRule,'nullable'],
+            '*.country'=>[new FilterStringRule , 'nullable'],
+            '*.city'=>[new FilterStringRule , 'nullable'], 
+            '*.address' =>[new FilterStringRule , 'nullable'],
+            '*.postal_code' =>[new FilterStringRule , 'nullable'],
+            '*.unit_no' =>[new FilterStringRule , 'nullable'],
+            '*.lat' =>[new FilterStringRule , 'nullable'],
+            // '*.lng' =>['string','nullable'],
+            '*.rep_first_name'=>[new FilterStringRule , 'nullable'],
+            '*.rep_phone_number'=>['numeric','not_in:0'], 
         ];
     }
 

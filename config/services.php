f05f0955f8eb31fcee7bd03afd5f1b6594330373
mohammed khalaf <mohammedkhalaf@mohammeds-MacBook-Air.local>
2020-08-18 18:32:25 +0200

    google login url

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'google' => [
         'client_id' => '361120492376-7qgod4o5ttfqhvm8fbarq20ok1tdvtpe.apps.googleusercontent.com',
         'client_secret' => '85a5u9SwSS_zHc474el5EO1O',
-        'redirect' => 'http://ec2-18-184-253-213.eu-central-1.compute.amazonaws.com:3000/auth/google/callback',
+        'redirect' => 'http://ec2-18-159-177-129.eu-central-1.compute.amazonaws.com:3000/auth/google/callback',
       ],
 
       'facebook' => [

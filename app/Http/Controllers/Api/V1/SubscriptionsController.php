bdb0ea87e0c6f53fbd2b660a88738d5cadda7335
NohaMahgoub <noha.mahgou@gmail.com>
2020-08-24 15:20:08 +0300

    change payment error

--- app/Http/Controllers/Api/V1/SubscriptionsController.php
+++ app/Http/Controllers/Api/V1/SubscriptionsController.php
@@ -34,41 +34,47 @@ class SubscriptionsController extends Controller
         //the plan
         $subscription =  Subscription::findOrFail($id);
         $planPrice= number_format($subscription->price,2, '.', '');
-        if(array_key_exists("id",$paymentObj)  && !empty($paymentObj['id']) && $paymentObj['amount']==$planPrice ) //id
-        {
-                    // this function contains (subscribe + change plan)
-                        $UserSubscription = SubscriptionDetail::where('user_id',auth()->guard('api')->user()->id)->get();
-                        if(!$UserSubscription->isEmpty()){   
-                                //if the user has old plan
-                                $oldSubscription = SubscriptionDetail::where('user_id',auth()->guard('api')->user()->id)->first();                               
-                                    // upgrade or downgrade the plan or re_subscribe in the same plan
-                                    $updatedPlan=SubscriptionDetail::changePlane($id,$paymentObj['id']); 
-                                   
-                                    $subscriber =  SubscriptionDetail::with('user')->where('id', $updatedPlan->id)->first();
-                                    //mail
-                                    Mail::to($subscriber->user->email)->send(new ReminderMail($subscriber,5,$subscription->name));
-                                    //pdf
-                                    $data = SubscriptionDetail::getSubscriptionData($subscriber,$subscription);
-                                    SubscriptionDetail::sendInvoicePdf($data); 
-                                    return response()->json(['updatedPlan'=> json_decode($updatedPlan) ,'message' => 'You are Successfully Subscribe to a New Plan..']);            
-                               
+        if($paymentObj['amount']==$planPrice){
+                if(array_key_exists("id",$paymentObj)  && !empty($paymentObj['id']) ) //id
+                {
+                            // this function contains (subscribe + change plan)
+                                $UserSubscription = SubscriptionDetail::where('user_id',auth()->guard('api')->user()->id)->get();
+                                if(!$UserSubscription->isEmpty()){   
+                                        //if the user has old plan
+                                        $oldSubscription = SubscriptionDetail::where('user_id',auth()->guard('api')->user()->id)->first();                               
+                                            // upgrade or downgrade the plan or re_subscribe in the same plan
+                                            $updatedPlan=SubscriptionDetail::changePlane($id,$paymentObj['id']); 
+                                        
+                                            $subscriber =  SubscriptionDetail::with('user')->where('id', $updatedPlan->id)->first();
+                                            //mail
+                                            Mail::to($subscriber->user->email)->send(new ReminderMail($subscriber,5,$subscription->name));
+                                            //pdf
+                                            $data = SubscriptionDetail::getSubscriptionData($subscriber,$subscription);
+                                            SubscriptionDetail::sendInvoicePdf($data); 
+                                            return response()->json(['updatedPlan'=> json_decode($updatedPlan) ,'message' => 'You are Successfully Subscribe to a New Plan..']);            
+                                    
 
-                        }else{
-                                //for new subscription 
-                                $newSubscription=SubscriptionDetail::newSubscription($id,$paymentObj['id']);
-                                $subscriber =  SubscriptionDetail::with('user')->where('id', $newSubscription->id)->first();
-                                //mail
-                                Mail::to($subscriber->user->email)->send(new ReminderMail($subscriber,4,$subscription->name));
-                                //pdf
-                                $data = SubscriptionDetail::getSubscriptionData($subscriber,$subscription);
-                                SubscriptionDetail::sendInvoicePdf($data);
-                                return response()->json(['updatedPlan'=> json_decode($newSubscription) ,'message' => 'You are Successfully Subscribe in a New Plan..']);            
-                           
-                            } 
-        }else{
-            $responseObj=json_decode($responseObj,true);
-            return $responseObj['result'];               
-        }
+                                }else{
+                                        //for new subscription 
+                                        $newSubscription=SubscriptionDetail::newSubscription($id,$paymentObj['id']);
+                                        $subscriber =  SubscriptionDetail::with('user')->where('id', $newSubscription->id)->first();
+                                        //mail
+                                        Mail::to($subscriber->user->email)->send(new ReminderMail($subscriber,4,$subscription->name));
+                                        //pdf
+                                        $data = SubscriptionDetail::getSubscriptionData($subscriber,$subscription);
+                                        SubscriptionDetail::sendInvoicePdf($data);
+                                        return response()->json(['updatedPlan'=> json_decode($newSubscription) ,'message' => 'You are Successfully Subscribe in a New Plan..']);            
+                                
+                                    } 
+                }else{
+                    $responseObj=json_decode($responseObj,true);
+                    return response()->json($responseObj['result'],422);
+                    
+                }
+            }else{
+                return response()->json(['error'=>'amount missmatch'],422);
+                
+            }
 
     }
 

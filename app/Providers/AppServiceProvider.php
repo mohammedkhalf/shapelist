fe65d4aa49577c5a0971b1fabee4b7ff410aa9e4
NohaMahgoub <noha.mahgou@gmail.com>
2020-08-27 16:22:44 +0300

    test2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,10 +17,6 @@ class AppServiceProvider extends ServiceProvider
         if(config('app.env') === 'production') {
             \URL::forceScheme('https');
         }
-        
-        if(env('REDIRECT_HTTPS')) {
-            $url->forceScheme('https');
-        }
         /*
          * Application locale defaults for various components
          *
